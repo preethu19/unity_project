8fb31372c4d1c1fe19dddac202de0652a86a0ce3
preethu19 <preethamkumar2000@gmail.com>
2022-04-27 23:32:05 +0530

    udp connection added

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -5,17 +5,126 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-
+using System.Threading;
 
 
 public class MoveController : MonoBehaviour
 {
+    public bool moveForward;
 
-	public Transform vrCamera;
+    // UDP Code
+
+    [HideInInspector] public bool isTxStarted = false;
+
+    [SerializeField] string IP = "127.0.0.1"; // local host
+    [SerializeField] int rxPort = 8000; // port to receive data from Python on
+    [SerializeField] int txPort = 8001; // port to send data to Python on
+
+    //int i = 0; // DELETE THIS: Added to show sending data from Unity to Python via UDP
+
+    // Create necessary UdpClient objects
+    UdpClient client;
+    IPEndPoint remoteEndPoint;
+    Thread receiveThread; // Receiving Thread
+
+    //IEnumerator SendDataCoroutine() // DELETE THIS: Added to show sending data from Unity to Python via UDP
+    //{
+    //    while (true)
+    //    {
+    //        SendData("Sent from Unity: " + i.ToString());
+    //        i++;
+    //        yield return new WaitForSeconds(1f);
+    //    }
+    //}
+
+    public void SendData(string message) // Use to send data to Python
+    {
+        try
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            client.Send(data, data.Length, remoteEndPoint);
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+    }
+
+    void Awake()
+    {
+        // Create remote endpoint (to Matlab) 
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), txPort);
+
+        // Create local client
+        client = new UdpClient(rxPort);
+
+        // local endpoint define (where messages are received)
+        // Create a new thread for reception of incoming messages
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+        // Initialize (seen in comments window)
+        print("UDP Comms Initialised");
+
+        //StartCoroutine(SendDataCoroutine()); // DELETE THIS: Added to show sending data from Unity to Python via UDP
+    }
+
+    // Receive data, update packets received
+    private void ReceiveData()
+    {
+        while (true)
+        {
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+                string text = Encoding.UTF8.GetString(data);
+                print(">> " + text);
+                if (text.Equals("forward"))
+                {
+                    moveForward = true;
+                }
+                else
+                {
+                    moveForward = false;
+                }
+                ProcessInput(text);
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    private void ProcessInput(string input)
+    {
+        // PROCESS INPUT RECEIVED STRING HERE
+
+        if (!isTxStarted) // First data arrived so tx started
+        {
+            isTxStarted = true;
+        }
+    }
+
+    //Prevent crashes - close clients and threads properly!
+    void OnDisable()
+    {
+        if (receiveThread != null)
+            receiveThread.Abort();
+
+        client.Close();
+    }
+
+
+
+
+    public Transform vrCamera;
 	public Transform cart;
 	public float toggleAngle = 30.0f;
 	public float speed = 3.0f;
-	public bool moveForward;
+	
 	public bool moveBackward = false;
 	
 	
@@ -44,14 +153,14 @@ public class MoveController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
-		{
-			moveForward = true;
-		}
-		else
-		{
-			moveForward = false;
-		}
+		//if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
+		//{
+		//	moveForward = true;
+		//}
+		//else
+		//{
+		//	moveForward = false;
+		//}
 
 		// cart code
 		a = vrCamera.eulerAngles.y;
